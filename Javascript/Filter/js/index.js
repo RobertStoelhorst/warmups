1419834744140e83a76b1b23526c76ecd6e90e66
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2020-12-10 10:38:12 +1100

    includes banana bonus

--- Javascript/Filter/js/index.js
+++ Javascript/Filter/js/index.js
@@ -2,19 +2,22 @@ console.log("is this working")
 
 
 const removeDuplicates = function (words) {
-    const splitWords = words.split(' ');
-    const uniqueWords = [];
+    const splitWords = words.split(' '); // split words into an array
+    console.log(splitWords);
+    const uniqueWords = []; // declare empty array
 
-    for(let i = 0; i < splitWords.length; i++) {
+    for(let i = 0; i < splitWords.length; i++) { 
         if (uniqueWords.includes(splitWords[i]) === false) {
-            uniqueWords.push(splitWords[i])
+            uniqueWords.push(splitWords[i]) // loop through and push to unique words array only the unique words
         }
     }
 
-        return uniqueWords
+        return uniqueWords;
 }
 
 console.log(removeDuplicates('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'));
+
+// =--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 const banana = function () {
     const result = 'b' + 'a' + 'b' * 'a' + 'a' // write an equation that returns NaN and place the strings either side
