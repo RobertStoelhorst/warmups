a46baa7531079de641d4cf1272857cad949bc17c
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2020-12-22 10:48:39 +1100

    sum arry and dom manip

---
+++ Javascript/sum-array/js/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+    console.log("is this ready")
+
+    // const sumArray = function (array) {
+    //     console.log(array[0], array[1]);
+
+    //     let sum = 0;
+
+    //     for(let i = 0; i < array[0].length; i++) {
+    //         sum + array[i];
+    //     }
+
+    //     return sum;
+    // };
+
+    // console.log(sumArray([[1, 3, 3],[1, 2, 3]]));
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//  SOLUTION
+
+    // const sumArray = function (array1, array2) {
+    //     console.log(array1, array2);
+    //     let sum = 0;
+        
+    //     for(let i = 0; i < array1.length; i++) {
+    //         console.log([i]);
+    //         sum += array1[i];
+    //     }
+
+    //     for(let i = 0; i < array2.length; i++) {
+    //         sum += array2[i];
+    //     }
+
+    //     return sum;
+    // }
+
+    // console.log(sumArray([1, 2, 3], [2, 3, 4]))
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+// BONUS: multiple array sum
+
+const multiArraySum = function () {
+    let sum = 0;
+
+    for(let i = 0; i < arguments.length; i++) {
+        for(let j = 0; j < arguments[i].length; j++) {
+            sum += arguments[i][j];
+        }
+    }
+    return sum;
+}
+
+console.log(multiArraySum([6, 3, 1], [4, 8, 3.3]))
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+});
