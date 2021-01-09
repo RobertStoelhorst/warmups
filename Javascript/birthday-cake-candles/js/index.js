39cbe8196e885dab8ca72274a2df652e16353dfb
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-01-09 23:22:23 +1100

    hackerank Birthday Cake Candles

--- Javascript/birthday-cake-candles/js/index.js
+++ Javascript/birthday-cake-candles/js/index.js
@@ -25,12 +25,33 @@ function birthdayCakeCandles(n, arr) {
         }    
     } 
     console.log(count);
-    // return count;
+    return count;
 }
 
 // Be aware of time complexity. 
 // In my testcase 4, n was 10000. If you use a sorting 
 // algorithm with n^2, you will get a runtime error.
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// If we use two loops it will pass but i'm not happy with this function
+
+// function birthdayCakeCandles(n, candles) {
+//     let max = 0;
+//     for(let i = 0; i < candles.length; i++) {
+//         if(candles[i] > max) {
+//             max = candles[i];
+//         }
+//     }
+//     let count = 0;
+//     for(let i = 0; i < candles.length; i++) {
+//         if(candles[i] === max) {
+//             count++;
+//         }
+//     }
+//     console.log(count);
+//     return count;
+//     }
 
 birthdayCakeCandles(2, [3, 2, 1, 3, 3, 1, 3, 3, 0]);
 
