5393280e91cf36661b832f867a4d3886380267ea
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-01-20 09:39:07 +1100

    javascript and Ruby solution

---
+++ Javascript/roman-numerals/js/index.js
@@ -0,0 +1,40 @@
+console.log("Roman Numerals");
+
+const roman = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+
+// console.log(roman.IV);
+
+const romanNumeral = function (num) {
+  // loop less if we have a condition where if the number passed in is 0
+  // it will return and exit function
+  if (num === 0) {
+    return "0 is not a Roman number";
+  }
+  let str = "";
+
+  //   loop over object
+  for (let i of Object.keys(roman)) {
+    let q = Math.floor(num / roman[i]);
+    num -= q * roman[i];
+    str += i.repeat(q);
+  }
+  return str;
+};
+
+console.log(romanNumeral(0));
+console.log(romanNumeral(8));
+console.log(romanNumeral(1001));
