1b717ef07b54ccd6d7bc68687bdddf0a2fcbb059
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-01-29 11:46:48 +1100

    hacckerank jumping on clouds

--- Javascript/jumping-on-the-clouds/js/index.js
+++ Javascript/jumping-on-the-clouds/js/index.js
@@ -4,19 +4,19 @@ console.log("Jumping On The Clouds");
 //  My solution !
 
 const cloudJump = function (n, arr) {
-  let jumpsMax = 0,
-    jumpsMin = 0;
+  let jumpsMin = 0;
 
   for (let i = 0; i < arr.length - 1; i++) {
     // console.log(arr[i]);
-    if (arr[i] === arr[i + 1] && arr[i] === arr[i + 2]) {
-      jumpsMax += 2;
-    } else if (arr[i] === 0) {
+    if (arr[i] === 0) {
+      if (arr[i] === arr[i + 1]) {
+        jumpsMin++;
+        i++;
+      }
+    } else {
       jumpsMin++;
     }
   }
-  //   return `total maximum jumps = ${jumpsMax + jumpsMin}`;
-  console.log(`total maximum jumps = ${jumpsMax + jumpsMin}`);
   //   return `total minimum jumps = ${jumpsMin}`;
   console.log(`total minimum jumps = ${jumpsMin}`);
 };
@@ -30,6 +30,8 @@ cloudJump(7, [0, 0, 1, 0, 0, 1, 0]);
 // OHHH that one was good, it hurt my brain when I
 // was reading the setup for this test but it wasn't
 // that hard in the end
+
+// it has not passed all test cases so back to thedrawing board
 
 // =-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-
 // credit to Artem Bozhko for this solution I found on stack
