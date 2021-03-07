4382820280d0a61add2aaa4b55b481dd97bfe1fe
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-03-07 14:20:22 +1100

    hackerrank cat and mouse

---
+++ Javascript/cats-and-mouse/js/index.js
@@ -0,0 +1,15 @@
+console.log("Cats and a Mouse");
+
+catAndMouse = (x, y, z) => {
+  const catA = Math.abs(x - z);
+  const catB = Math.abs(y - z);
+  if (catB > catA) {
+    return "Cat A";
+  } else if (catB < catA) {
+    return "Cat B";
+  }
+  return "Mouse C";
+};
+
+console.log(catAndMouse(1, 2, 3));
+console.log(catAndMouse(1, 3, 2));
