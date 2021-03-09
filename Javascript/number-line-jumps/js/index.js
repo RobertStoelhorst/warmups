11e4908569575fc4a3bcc7fbcefc709c9398ddcf
RobertStoelhorst <robbiestoelhorst@hotmail.com>
2021-03-09 19:07:42 +1100

    hackerank number line jumps

--- Javascript/number-line-jumps/js/index.js
+++ Javascript/number-line-jumps/js/index.js
@@ -1,10 +1,10 @@
 console.log("Number Line Jumps");
 
 kangaroo = (x1, v1, x2, v2) => {
-  if ((x1 - v1) % (x2 - v2) == 0) {
-    return "NO";
-  } else {
+  if (v1 > v2 && (x2 - x1) % (v1 - v2) == 0) {
     return "YES";
+  } else {
+    return "NO";
   }
 };
 
